21bb462d6707670ecc2dcf4b979d3bd9612c14e1
Steve Spicklemire <steve@spvi.com>
2023-06-26 14:32:08 -0400

    add mongo/bunnet models

--- src/mongo_models.py
+++ src/mongo_models.py
@@ -1,52 +1,35 @@
 
-from google.cloud import ndb
+from datetime import datetime
+from bunnet import Document, TimeSeriesConfig, Granularity
+from pydantic import Field
 
-class User (ndb.Model):
+class User(Document):
     """A single user of the IDE"""
     # No parent
     # key is the user's unique name
-    joinDate = ndb.DateTimeProperty(auto_now_add=True)
-    email = ndb.StringProperty()
-    gaeUser = ndb.UserProperty()
-    secret = ndb.StringProperty()
+    joinDate: datetime = Field(default_factory=datetime.now)
+    email: str
+    secret: str
 
-class Folder (ndb.Model):
+    class Settings:
+        timeseries = TimeSeriesConfig(
+            time_field="joinDate", #  Required
+        )
+
+class Folder (Document):
     """A collection of programs created by a user"""
     # Parent is a User
     # key is the folder's name (unique for a user)
-    isPublic = ndb.BooleanProperty()
+    parentID: str
+    isPublic: bool
 
-class Program (ndb.Model):
+class Program (Document):
     """A single program"""
     # Parent is a Folder
     # key is the program's name (unique for a folder)
-    description = ndb.StringProperty()
-    source = ndb.TextProperty()
-    screenshot = ndb.BlobProperty()
-    datetime = ndb.DateTimeProperty() # this is UTC date and time
+    parentID: str
+    description: str
+    source: str
+    screenshot: bytes
+    datetime: datetime.date
 
-class Setting(ndb.Model):
-    """A setting value"""
-    # No parent
-    # Key is the setting name
-    # we're going to cache values since these will be
-    # static configuration values
-    #
-
-    value = ndb.StringProperty()
-    cache = {}
-
-    @staticmethod
-    def get(name):
-        NOT_SET_VALUE = "NOT SET"
-        ndb_setting = Setting.cache.get(name)
-
-        if not ndb_setting:
-            ndb_setting = ndb.Key("Setting",name).get()
-            if ndb_setting:
-                Setting.cache[name] = ndb_setting
-            else:
-                ndb_setting = Setting(id=name, value=NOT_SET_VALUE)
-                ndb_setting.put()
-
-        return ndb_setting
